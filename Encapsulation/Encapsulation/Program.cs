55f8ea9a1a0e865bd6ebe24a3e619b6b086c01ac
H F <jonis88@gmail.com>
2017-10-26 14:27:28 +0200

    3.4

--- Encapsulation/Encapsulation/Program.cs
+++ Encapsulation/Encapsulation/Program.cs
@@ -63,87 +63,94 @@ namespace Encapsulation
             }
 
             //Print all dogs in list
-            //foreach (var Animal in list.OfType<Dog>())
-            //{                
-            //    Console.WriteLine(Animal.GetType().Name + "\n" + Animal.Stats() + "\n" + Animal.Speak() + "\n");
+            Console.ReadKey();
+            Console.Clear();
+            foreach (var Animal in list.OfType<Dog>())
+            {
+                Console.WriteLine(Animal.GetType().Name + "\n" + Animal.Stats() + "\n" + Animal.Speak() + "\n");
 
-            //}
+            }
             Console.ReadKey();
             Console.Clear();
 
+            //Create list for UserError
             List<UserError> error = new List<UserError>();
-
+            
+            //Add to error list
             error.Add(new TextInputError());
             error.Add(new NumericInputError());
-
+            
+            //Loop for listing the errors
             foreach (var UserError in error)
             {
                 Console.WriteLine(UserError.UEMessage());
             }
             Console.ReadKey();
 
+            /// <summary>
+            /// Main method to get and print out all information of person
+            /// </summary>
+            /// <param name="args"></param>
+            bool continued = true;
+            //Instantiate a new Person, but could not directly access the variables, because they are private fields
+            //Person per = new Person();
+
+
+            do
+            {
+                //Instantiate a new PersonHandler
+                PersonHandler perhandler = new PersonHandler();
+
+                //Create and print person
+                continued = AddPersonFromConsole(perhandler);
+            }
+            while (continued);
+
+            Console.ReadKey();
         }
+
+
+        /// <summary>
+        /// Read person information inputed from console then add to an instance of PersonHandler
+        /// </summary>
+        /// <param name="personhandler">handle information of each person</param>
+        private static bool AddPersonFromConsole(PersonHandler personhandler)
+        {
+            Console.Write("Please input the first name:");
+            string fname = Console.ReadLine();
+            if (String.IsNullOrEmpty(fname)) return false;
+
+            Console.Write("Please input the last name:");
+            string lname = Console.ReadLine();
+            if (String.IsNullOrEmpty(lname)) return false;
+
+            Console.Write("Please input the age:");
+            int age = 0;
+            if (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("Age couldn't be parsed");
+            }
+
+            Console.Write("Please input the height:");
+            double height = 0;
+            if (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.InvariantCulture, out height))
+            {
+                Console.WriteLine("Height couldn't be parsed");
+            }
+
+            Console.Write("Please input the weight:");
+            double weight = 0;
+            if (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.InvariantCulture, out weight))
+            {
+                Console.WriteLine("Height couldn't be parsed");
+            }
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(personhandler.PrintPerson(age, fname, lname, height, weight));
+            return true;
+        }
+
+
     }
 }
 
 
-        /// <summary>
-        /// Main method to get and print out all information of person
-        /// </summary>
-        /// <param name="args"></param>
-        //    bool continued = true;
-        //    //Instantiate a new Person, but could not directly access the variables, because they are private fields
-        //    //Person per = new Person();
-            
-
-        //    do
-        //    {
-        //        //Instantiate a new PersonHandler
-        //        PersonHandler perhandler = new PersonHandler();
-
-        //        //Create and print person
-        //        continued = AddPersonFromConsole(perhandler);
-        //    }
-        //    while (continued);
-
-        //    Console.ReadKey();
-        //}
-
-
-        ///// <summary>
-        ///// Read person information inputed from console then add to an instance of PersonHandler
-        ///// </summary>
-        ///// <param name="personhandler">handle information of each person</param>
-        //private static bool AddPersonFromConsole(PersonHandler personhandler)
-        //{
-        //    Console.Write("Please input the first name:");
-        //    string fname = Console.ReadLine();
-        //    if (String.IsNullOrEmpty(fname)) return false;
-
-        //    Console.Write("Please input the last name:");
-        //    string lname = Console.ReadLine();
-        //    if (String.IsNullOrEmpty(lname)) return false;
-
-        //    Console.Write("Please input the age:");
-        //    int age = 0;
-        //    if (!int.TryParse(Console.ReadLine(), out age))
-        //    {
-        //        Console.WriteLine("Age couldn't be parsed");
-        //    }
-
-        //    Console.Write("Please input the height:");
-        //    double height = 0;
-        //    if (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.InvariantCulture, out height))
-        //    {
-        //        Console.WriteLine("Height couldn't be parsed");
-        //    }
-
-        //    Console.Write("Please input the weight:");
-        //    double weight = 0;
-        //    if (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.InvariantCulture, out weight))
-        //    {
-        //        Console.WriteLine("Height couldn't be parsed");
-        //    }
-        //    Console.WriteLine("---------------------------");
-        //    Console.WriteLine(personhandler.PrintPerson(age, fname, lname, height, weight));
-        //    return true;
